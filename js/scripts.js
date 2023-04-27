6733189cf950312a235a38166630720e78d74a12
Dinervc <50810420+Dinervc@users.noreply.github.com>
2023-04-27 10:05:01 +0200

    JS loop funktion

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+const quotes = [
+  "When the past comes knocking, don't answer. It has nothing new to tell you.",
+  "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+  "Believe you can and you're halfway there.",
+  "If you want to live a happy life, tie it to a goal, not to people or things.",
+  "Happiness is not something ready made. It comes from your own actions.",
+];
+
+const quoteContainer = document.querySelector(".quotecontainer");
+const quote = quoteContainer.querySelector(".quote");
+
+setInterval(() => {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  quote.textContent = quotes[randomIndex];
+}, 3000);
