49a5eeccef061a8ced8cce8df029416a3c822914
Dinervc <50810420+Dinervc@users.noreply.github.com>
2023-05-25 10:57:35 +0200

    Test

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,17 @@ let previousQuote = "";
 let wasPreviousSame = false;
 
 function fetchQuote() {
-  $.getJSON("data.php", function (data) {
-    if (data.error === -1) {
-      console.error("An error occurred while fetching quote data.");
-    } else {
-      if (previousQuote === data.quote) {
-        wasPreviousSame = true;
-        fetchQuote();
+    $.getJSON("data.php", function (data) {
+      if (data.error === -1) {
+        console.error("An error occurred while fetching quote data.");
       } else {
-        previousQuote = data.quote;
-
-        setTimeout(
-          function () {
-            $(".quote, .author").addClass("fade-out");
+        if (previousQuote === data.quote) {
+            wasPreviousSame = true;
+          fetchQuote(); 
+        } else {
+          previousQuote = data.quote;
+          $(".quote, .author").addClass("fade-out");
+          setTimeout(function () {
             $(".quote").text(data.quote);
             $(".author").text(data.author);
             $(".quote, .author").removeClass("fade-out");
@@ -23,12 +21,13 @@ function fetchQuote() {
           !wasPreviousSame ? 1000 : 0
         );
         console.log(data.quote + " + " + data.author);
+        }
       }
-    }
+    });
+  }
+  
+  $(document).ready(function () {
+    fetchQuote(); 
+    setInterval(fetchQuote, 5000); 
   });
-}
-
-$(document).ready(function () {
-  fetchQuote();
-  setInterval(fetchQuote, 5000);
-});
+  
